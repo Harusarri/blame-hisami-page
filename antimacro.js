7985f552b2df92a79b4961b2b0fb412875795baa
Harusarri <163049997+Harusarri@users.noreply.github.com>
2024-07-14 21:26:50 +0900

    Update antimacro.js

--- antimacro.js
+++ antimacro.js
@@ -1,23 +1,19 @@
 let clickCount = 0;
 let lastClickTime = 0;
-const MAX_CPS = 25; // 설정할 최대 CPS 값
+const MAX_CPS = 100; // 설정할 최대 CPS 값을 높입니다.
 
 window.addEventListener('userClick', (event) => {
     const currentTime = Date.now();
     const timeDiff = currentTime - lastClickTime;
 
     if (timeDiff < 1000 / MAX_CPS) {
-        console.warn('Possible autoclicker detected!');
-        // 클릭을 무시하지 않고 경고만 출력합니다.
+        console.warn('Too many clicks per second!');
+        clickCount = 0; // 클릭 수 초기화
+        return; // 클릭을 무시합니다.
     }
 
     lastClickTime = currentTime;
     clickCount++;
-    if (clickCount > MAX_CPS) {
-        console.warn('Too many clicks per second!');
-        clickCount = 0; // 클릭 수 초기화
-        return;
-    }
 
     fetch('https://blamebackend.onrender.com/api/click', {
         method: 'POST',
